32705b94cb847de0ad9b99ff235bb93b6583d2a6
Amit Kumar Swami <aks.swami@live.com>
2022-06-09 22:24:20 +0800

    Initial commit, Xcode project created

---
+++ Shared/xenShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  xenShopApp.swift
+//  Shared
+//
+//  Created by Amit Kumar Swami on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct xenShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
